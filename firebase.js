b3b938da921765927ec28c82d06ce33bfa7a181f
amitroyrajputma-beep <amitroyrajputma@gmail.com>
2026-02-02 16:00:42 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// firebase.js
+var firebaseConfig = {
+  apiKey: "AIzaSyBpcY72lQEziKJBdMWCgOIxWKLwIWe1Fgw",
+  databaseURL: "https://royrohit-294fc-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "royrohit-294fc"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
